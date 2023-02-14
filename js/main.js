afbf1c3c85d00b318e13dc76bf0c369f2616539f
Brenda Trieu <trieubrenda@gmail.com>
2023-02-14 18:28:06 +0000

    Created function to render new entries.

--- js/main.js
+++ js/main.js
@@ -23,5 +23,37 @@ var logNewEntry = event => {
   $form.reset();
 };
 
+var renderEntry = entry => {
+  var $entryList = document.querySelector('ul');
+
+  // Create all new elements
+  var $entryLi = document.createElement('li');
+  var $liDiv = document.createElement('div');
+  var $imgDiv = document.createElement('div');
+  var $imgEl = document.createElement('img');
+  var $textDiv = document.createElement('div');
+  var $imgTitle = document.createElement('h4');
+  var $imgNotes = document.createElement('p');
+
+  // Assign classes and content to elements
+  $liDiv.className = 'row';
+  $imgDiv.className = 'column-half';
+  $imgEl.setAttribute('src', entry.photoUrl);
+  $textDiv.className = 'column-half';
+  $imgTitle.textContent = entry.title;
+  $imgNotes.textContent = entry.notes;
+
+  // Append elements
+  $imgDiv.appendChild($imgEl);
+  $textDiv.appendChild($imgTitle);
+  $textDiv.appendChild($imgNotes);
+  $liDiv.appendChild($imgDiv);
+  $liDiv.appendChild($textDiv);
+  $entryLi.appendChild($liDiv);
+  $entryList.appendChild($entryLi);
+};
+
+renderEntry();
+
 $form.addEventListener('submit', logNewEntry);
 $imgUrlField.addEventListener('input', livePreview);

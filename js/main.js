4ec93878ded99b8ab782bd3784289e6524c5c8cb
Brenda Trieu <trieubrenda@gmail.com>
2023-02-14 03:21:14 +0000

    Created submit function to store entries and reset page.

--- js/main.js
+++ js/main.js
@@ -10,12 +10,14 @@ var $form = document.querySelector('form');
 
 $form.addEventListener('submit', event => {
   event.preventDefault();
+
   var $newEntry = {
     title: $form.elements.title.value,
     photoUrl: $form.elements['img-src'].value,
     notes: $form.elements['img-notes'].value,
     entryId: data.nextEntryId
   };
+
   data.nextEntryId++;
   data.entries.push($newEntry);
   $imgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
